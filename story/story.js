4663e3036f8fb956225da8ff713042318265290c
jarlo72 <91274246+jarlo72@users.noreply.github.com>
2022-01-27 22:14:09 -0500

    Update story.js

--- story/story.js
+++ story/story.js
@@ -1,1 +1,63 @@
+function initMap() {
+  const directionsRenderer = new google.maps.DirectionsRenderer();
+  const directionsService = new google.maps.DirectionsService();
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 13,
+    center: { lat: 43.476176, lng: -80.524947 }
+  });
+  
+  const contentString =
+    '<div id="content">' +
+    '<div id="siteNotice">' +
+    "</div>" +
+    '<h1 id="firstHeading" class="firstHeading">Shawerma Plus</h1>' +
+    '<div id="bodyContent">' +
+    "<p>Get the Falafal wrap jawad's style</p>" +
+    "</div>" +
+    "</div>";
+  
+  const infowindow = new google.maps.InfoWindow({
+    content: contentString,
+  });
+  const marker = new google.maps.Marker({
+    position: "ChIJ816IxAb0K4gRYpbXJOgIt1Y",
+    map,
+    title: "Shawerma Plus",
+  });
 
+  marker.addListener("click", () => {
+    infowindow.open({
+      anchor: marker,
+      map,
+      shouldFocus: false,
+    });
+  });  
+  
+  directionsRenderer.setMap(map);
+  calculateAndDisplayRoute(directionsService, directionsRenderer);
+  document.getElementById("mode").addEventListener("change", () => {
+    calculateAndDisplayRoute(directionsService, directionsRenderer);
+  });
+}
+
+function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+  const selectedMode = document.getElementById("mode").value;
+
+  directionsService
+    .route({
+      origin: "ChIJ816IxAb0K4gRYpbXJOgIt1Y",
+      destination: "ChIJp1-W86X0K4gR_GMElbM3KYQ",
+      // Note that Javascript allows us to access the constant
+      // using square brackets and a string value as its
+      // "property."
+      waypoints: [
+        { location: "ChIJ325EafTzK4gR0GbMvhc3XTs" },
+        { location: "ChIJbXnSHSrzK4gRNvJAYl8_Rv8" },
+      ],
+      travelMode: google.maps.TravelMode[selectedMode],
+    })
+    .then((response) => {
+      directionsRenderer.setDirections(response);
+    })
+    .catch((e) => window.alert("Directions request failed due to " + status));
+}
